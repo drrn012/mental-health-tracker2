9d7928d6f27c41c55fe7c9aa8c6e48d773373256
Derensh Pandian <70104051+drrn012@users.noreply.github.com>
2024-09-20 15:07:11 +0700

    Add failing test for page title

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from .models import MoodEntry
 
 class MainTest(TestCase):
+    def test_main_template_uses_correct_page_title(self):
+        response = Client().get("/")
+        hmtl_response = response.content.decode("utf8")
+        self.assertIn("PBD Mental Health Tracker", hmtl_response)
+
     def test_main_url_is_exist(self):
         response = Client().get('')
         self.assertEqual(response.status_code, 200)
