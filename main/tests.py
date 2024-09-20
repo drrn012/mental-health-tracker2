69c164fa780f20d1abb7660a4ca1be75d5229bd1
Derensh Pandian <70104051+drrn012@users.noreply.github.com>
2024-09-20 15:12:54 +0700

    Add failing test for page title

--- main/tests.py
+++ main/tests.py
@@ -5,8 +5,8 @@ from .models import MoodEntry
 class MainTest(TestCase):
     def test_main_template_uses_correct_page_title(self):
         response = Client().get("/")
-        hmtl_response = response.content.decode("utf8")
-        self.assertIn("PBD Mental Health Tracker", hmtl_response)
+        html_response = response.content.decode("utf8")
+        self.assertIn("PBD Mental Health Tracker", html_response)
 
     def test_main_url_is_exist(self):
         response = Client().get('')
