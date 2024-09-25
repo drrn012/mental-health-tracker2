d1c68f73710e223eefa65c500a83ce8c1749437d
Derensh Pandian <70104051+drrn012@users.noreply.github.com>
2024-09-25 11:59:53 +0700

    pws tutorial

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
+#test
 class ExampleFunctionalTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
