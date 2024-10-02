8ad37469966646ca201cd1b17b630a7bc43e3a1f
drrn012 <pandianderensh@gmail.com>
2024-10-02 11:36:13 +0700

    tutorial 5

--- main/views.py
+++ main/views.py
@@ -9,20 +9,20 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+
 
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
     context = {
         'npm': '2306256311',
         'name': request.user.username,
         'class': 'PBP KKI',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES.get('last_login', 'Not set'),  # Default value if cookie doesn't exist
     }
-
     return render(request, "main.html", context)
-
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
@@ -37,11 +37,11 @@ def create_mood_entry(request):
     return render(request, "create_mood_entry.html", context)
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)  # Update to filter by user
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -65,25 +65,22 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-      if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-      
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -92,14 +89,10 @@ def logout_user(request):
     return response
 
 def edit_mood(request, id):
-    # Get mood entry based on id
-    mood = MoodEntry.objects.get(pk = id)
-
-    # Set mood entry as an instance of the form
+    mood = MoodEntry.objects.get(pk=id)
     form = MoodEntryForm(request.POST or None, instance=mood)
 
     if form.is_valid() and request.method == "POST":
-        # Save form and return to home page
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -107,9 +100,24 @@ def edit_mood(request, id):
     return render(request, "edit_mood.html", context)
 
 def delete_mood(request, id):
-    # Get mood based on id
-    mood = MoodEntry.objects.get(pk = id)
-    # Delete mood
+    mood = MoodEntry.objects.get(pk=id)
     mood.delete()
-    # Return to home page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood"))  # Strip HTML tags
+    feelings = strip_tags(request.POST.get("feelings"))  # Strip HTML tags
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, 
+        feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
